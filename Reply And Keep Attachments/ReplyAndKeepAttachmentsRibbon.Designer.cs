bf04040d4208f036d068e7ff3a704cd036da4526
Levoment <levoment@users.noreply.github.com>
2019-06-01 20:51:08 -0500

    Add project files.

---
+++ Reply And Keep Attachments/ReplyAndKeepAttachmentsRibbon.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Reply_And_Keep_Attachments
+{
+    partial class ReplyAndKeepAttachmentsRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public ReplyAndKeepAttachmentsRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.replyAndKeepAttachmentsTab1 = this.Factory.CreateRibbonTab();
+            this.replyAndKeepAttachmentsGroup1 = this.Factory.CreateRibbonGroup();
+            this.replyAndKeepAttachmentsButton = this.Factory.CreateRibbonButton();
+            this.replyAllAndKeepAttachmentsButton = this.Factory.CreateRibbonButton();
+            this.replyAndKeepAttachmentsTab1.SuspendLayout();
+            this.replyAndKeepAttachmentsGroup1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // replyAndKeepAttachmentsTab1
+            // 
+            this.replyAndKeepAttachmentsTab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.replyAndKeepAttachmentsTab1.Groups.Add(this.replyAndKeepAttachmentsGroup1);
+            this.replyAndKeepAttachmentsTab1.Label = "Reply and Keep Attachments";
+            this.replyAndKeepAttachmentsTab1.Name = "replyAndKeepAttachmentsTab1";
+            // 
+            // replyAndKeepAttachmentsGroup1
+            // 
+            this.replyAndKeepAttachmentsGroup1.Items.Add(this.replyAndKeepAttachmentsButton);
+            this.replyAndKeepAttachmentsGroup1.Items.Add(this.replyAllAndKeepAttachmentsButton);
+            this.replyAndKeepAttachmentsGroup1.Label = "Reply and Keep Attachments";
+            this.replyAndKeepAttachmentsGroup1.Name = "replyAndKeepAttachmentsGroup1";
+            // 
+            // replyAndKeepAttachmentsButton
+            // 
+            this.replyAndKeepAttachmentsButton.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.replyAndKeepAttachmentsButton.Image = global::Reply_And_Keep_Attachments.Properties.Resources.Reply_And_Keep_Attachments_Icon;
+            this.replyAndKeepAttachmentsButton.Label = "Reply and Keep Attachments";
+            this.replyAndKeepAttachmentsButton.Name = "replyAndKeepAttachmentsButton";
+            this.replyAndKeepAttachmentsButton.ShowImage = true;
+            this.replyAndKeepAttachmentsButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ReplyAndKeepAttachmentsButton_Click);
+            // 
+            // replyAllAndKeepAttachmentsButton
+            // 
+            this.replyAllAndKeepAttachmentsButton.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.replyAllAndKeepAttachmentsButton.Image = global::Reply_And_Keep_Attachments.Properties.Resources.Reply_All_And_Keep_Attachments_Icon;
+            this.replyAllAndKeepAttachmentsButton.Label = "Reply All and Keep Attachments";
+            this.replyAllAndKeepAttachmentsButton.Name = "replyAllAndKeepAttachmentsButton";
+            this.replyAllAndKeepAttachmentsButton.ShowImage = true;
+            this.replyAllAndKeepAttachmentsButton.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ReplyAllAndKeepAttachmentsButton_Click);
+            // 
+            // ReplyAndKeepAttachmentsRibbon
+            // 
+            this.Name = "ReplyAndKeepAttachmentsRibbon";
+            this.RibbonType = "Microsoft.Outlook.Mail.Read";
+            this.Tabs.Add(this.replyAndKeepAttachmentsTab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.ReplyAndKeepAttachmentsRibbon_Load);
+            this.replyAndKeepAttachmentsTab1.ResumeLayout(false);
+            this.replyAndKeepAttachmentsTab1.PerformLayout();
+            this.replyAndKeepAttachmentsGroup1.ResumeLayout(false);
+            this.replyAndKeepAttachmentsGroup1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab replyAndKeepAttachmentsTab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup replyAndKeepAttachmentsGroup1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton replyAndKeepAttachmentsButton;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton replyAllAndKeepAttachmentsButton;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal ReplyAndKeepAttachmentsRibbon ReplyAndKeepAttachmentsRibbon
+        {
+            get { return this.GetRibbon<ReplyAndKeepAttachmentsRibbon>(); }
+        }
+    }
+}
---
+++ Reply And Keep Attachments/ReplyAndKeepAttachmentsRibbon.cs
@@ -0,0 +1,136 @@
+﻿using System.IO;
+using System.Windows;
+using Microsoft.Office.Interop.Outlook;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace Reply_And_Keep_Attachments
+{
+    public partial class ReplyAndKeepAttachmentsRibbon
+    {
+        // Variable for controlling whether the reply all button gets clicked
+        private bool replyAll = false;
+
+        private void ReplyAndKeepAttachmentsRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void ReplyAndKeepAttachmentsButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            // Set the replyAll variable
+            replyAll = false;
+            replyAndKeepAttachments(sender, e);
+        }
+
+        private void replyAndKeepAttachments(object sender, RibbonControlEventArgs e)
+        {
+            // Get the inspector
+            Inspector inspector = e.Control.Context as Inspector;
+            // Get the current mail message
+            MailItem mailItem = inspector.CurrentItem as MailItem;
+
+            // Check if we have a mail message to work with
+            if (mailItem != null)
+            {
+                // Check if we have attachments
+                if (mailItem.Attachments.Count > 0)
+                {
+                    // Variable holding the reply
+                    MailItem mailItemToSend;
+
+                    // Check if Reply All button was pressed
+                    if (replyAll)
+                    {
+                        // Create the reply all message item
+                        mailItemToSend = mailItem.ReplyAll();
+                    }
+                    else
+                    {
+                        // Create the reply message item
+                        mailItemToSend = mailItem.Reply();
+                    }
+
+                    // Iterate through each attachment
+                    foreach (Attachment attachment in mailItem.Attachments)
+                    {
+                        try
+                        {
+                            string PR_ATTACH_FLAGS = "http://schemas.microsoft.com/mapi/proptag/0x37140003";
+                            // Get property of attachment (ATT_INVISIBLE_IN_HTML | ATT_INVISIBLE_IN_RTF)
+                            var attachmentFlags = attachment.PropertyAccessor.GetProperty(PR_ATTACH_FLAGS);
+                            // If attachment is not ATT_MHTML_REF
+                            if (attachmentFlags != 4)
+                            {
+                                // Path of the folder to save files before attaching
+                                string folderPath = @"C:\ReplyKeepAttachmentsTmp";
+                                try
+                                {
+                                    // Check if the folder does not exist
+                                    if (!Directory.Exists(folderPath))
+                                    {
+                                        // Create the folder if it doesn't exist
+                                        DirectoryInfo directoryInfo = Directory.CreateDirectory(folderPath);
+                                    }
+
+                                    // Check if the attachment is not an embeeded element
+                                    if ((int)attachment.Type != 6)
+                                    {
+                                        // Save current attachment
+                                        attachment.SaveAsFile(Path.Combine(@"C:\ReplyKeepAttachmentsTmp\", attachment.FileName));
+                                        if (mailItemToSend != null)
+                                        {
+                                            // Attach it to the reply message
+                                            mailItemToSend.Attachments.Add(Path.Combine(@"C:\ReplyKeepAttachmentsTmp\", attachment.FileName));
+                                            //mailItemToSend.Save();
+                                            // Delete the temporary file
+                                            if (File.Exists(Path.Combine(@"C:\ReplyKeepAttachmentsTmp\", attachment.FileName)))
+                                            {
+                                                File.Delete(Path.Combine(@"C:\ReplyKeepAttachmentsTmp\", attachment.FileName));
+                                            }
+                                        }
+                                    }
+
+                                }
+                                catch (System.IO.IOException ioException)
+                                {
+                                    // Show a message showing that there was an error while attempting to create the temporary folder
+                                    MessageBox.Show("There was an error while attempting to create the temporary folder to save attachments:\n\n" +
+                                        ioException.Message, "Reply and Keep Attachments Add-In Message", MessageBoxButton.OK, MessageBoxImage.Error);
+                                }
+                                catch (System.Exception genericException)
+                                {
+                                    // Show the exception message
+                                    MessageBox.Show(genericException.Message, "Reply and Keep Attachments Add-In Message", MessageBoxButton.OK, MessageBoxImage.Error);
+                                }
+                                finally { }
+                            }
+                        }
+                        catch (System.Runtime.InteropServices.COMException comException)
+                        {
+                            MessageBox.Show(comException.Message, "Reply and Keep Attachments Add-In Message", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+
+                    }
+                    // Check if the message to send is valid
+                    if (mailItemToSend != null)
+                    {
+                        // Open the reply Window to allow the user to edit and send the message
+                        mailItemToSend.GetInspector.Activate();
+                    }
+                }
+                else
+                {
+                    // Show a message saying that no attachments could be found
+                    MessageBox.Show("No attachments could be found on this mail message.", "Reply and Keep Attachments Add-In Message", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                }
+            }
+        }
+
+        private void ReplyAllAndKeepAttachmentsButton_Click(object sender, RibbonControlEventArgs e)
+        {
+            // Set the replyAll variable
+            replyAll = true;
+            replyAndKeepAttachments(sender, e);
+        }
+    }
+}
